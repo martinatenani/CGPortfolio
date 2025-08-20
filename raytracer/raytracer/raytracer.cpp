b64a684295148a6d41e7af4430beee695acc8dd2
Martina Tenani <martinatenani@gmail.com>
2025-08-20 16:38:49 +0200

    Fixed refraction issue

--- raytracer/raytracer/raytracer.cpp
+++ raytracer/raytracer/raytracer.cpp
@@ -96,25 +96,25 @@ class Sphere
 //[/comment]
 #define MAX_RAY_DEPTH 5
 
-float mix(const float &a, const float &b, const float &mix){return b*mix+a*(1-mix);}
+float mix(const float &a, const float &b, const float &mix)
+{
+    return b * mix + a * (1-mix);
+}
 
 Vec3f trace(
     const Vec3f &rayorig,
     const Vec3f &raydir,
     const std::vector<Sphere> &spheres,
-    const int depth)
+    const int &depth)
 {
     float tnear = INFINITY;
     const Sphere* sphere = NULL; //the sphere we are considering for rendering (?)
-    //Iterate on the spheres. Find the intersections with each of them.
-    for (unsigned int i = 0; i < spheres.size(); i++)
-    {
+    //Iterate on the spheres. Find the intersections (hits) with each of them.
+    for (unsigned i = 0; i < spheres.size(); ++i){
         float t0 = INFINITY, t1 = INFINITY;
-        if (spheres[i].intersect(rayorig, raydir, t0, t1))
-        {
+        if (spheres[i].intersect(rayorig, raydir, t0, t1)){
             if (t0<0) t0 = t1;
-            if (t0 < tnear)
-            {
+            if (t0 < tnear){
                 tnear = t0;
                 sphere = &spheres[i];
             }
@@ -134,33 +134,34 @@ Vec3f trace(
     // and not reflective) we check first for reflections.
     if ((sphere->transparency > 0 || sphere->reflection > 0) && depth < MAX_RAY_DEPTH) {
         float facingratio = -raydir.dot(nhit);
-        float fresneleffect = mix(pow(1-facingratio,3), 1, 0.1);
-        Vec3f refldir = raydir - nhit * 2 *raydir.dot(nhit);
+        float fresneleffect = mix(pow(1-facingratio,3), 1, 0.1); //evaluate refraction of light with fresnel effect
+        Vec3f refldir = raydir - nhit * 2 * raydir.dot(nhit);
         refldir.normalize();
-        Vec3f reflection = trace(phit + nhit*bias, refldir, spheres, depth + 1);
+        Vec3f reflection = trace(phit + nhit * bias, refldir, spheres, depth + 1);
         Vec3f refraction = 0;
-        //Sphere is also transparent
-        if (sphere -> transparency){
-            float ior = 1.1, eta = (inside) ? ior : 1/ior; //Inside or outside the surface?
-            float cosi = nhit.dot(raydir);
-            float k = 1 - eta*eta*(1-cosi*cosi);
-            Vec3f refdir = raydir*eta+nhit*(eta*cosi-sqrt(k));
-            refdir.normalize();
-            refraction = trace(phit - nhit*bias, refldir, spheres, depth + 1);
+        //Sphere is also transparent. Compute refraction ray (transmission)
+        if (sphere->transparency) {
+            float ior = 1.1, eta = (inside) ? ior : 1 / ior; // are we inside or outside the surface?
+            float cosi = -nhit.dot(raydir);
+            float k = 1 - eta * eta * (1 - cosi * cosi);
+            Vec3f refrdir = raydir * eta + nhit * (eta *  cosi - sqrt(k));
+            refrdir.normalize();
+            refraction = trace(phit - nhit * bias, refrdir, spheres, depth + 1);
         }
         //Mix reflection and refraction for the result
-        surfaceColor = (reflection * fresneleffect + refraction * (1-fresneleffect)*sphere->transparency) * sphere->surfaceColor;
+        surfaceColor = (
+            reflection * fresneleffect +
+            refraction * (1-fresneleffect)*sphere->transparency) * sphere->surfaceColor;
     }else //Sphere is opaque (or depth exceedes MAX_RAY_DEPTH)
     {
         //Cut down some calculations by not raytracing recursively
-        for (unsigned int i = 0; i < spheres.size(); i++)
-        {
-            if (spheres[i].emissionColor.x > 0)
-            {
+        for (unsigned i = 0; i < spheres.size(); ++i){
+            if (spheres[i].emissionColor.x > 0){
+                //This is a light
                 Vec3f transmission = 1;
                 Vec3f lightDirection = spheres[i].center - phit;
                 lightDirection.normalize();
-                for (unsigned int j = 0; j < spheres.size(); j++){
+                for (unsigned j = 0; j < spheres.size(); ++j){
                     if (i!=j){
                         float t0, t1;
                         if (spheres[j].intersect(phit + nhit * bias, lightDirection, t0, t1)){
@@ -169,38 +170,39 @@ Vec3f trace(
                         }
                     }
                 }
-                surfaceColor += sphere->surfaceColor * transmission * std::max(float(0), nhit.dot(lightDirection)) * spheres[i].emissionColor;
+                surfaceColor += sphere->surfaceColor * transmission *
+                    std::max(float(0), nhit.dot(lightDirection)) * spheres[i].emissionColor;
             }
         }
     }
-
     return surfaceColor + sphere->emissionColor;
 }
 
-//[comment]
-// Main rendering function. We compute a camera ray for each pixel of the image
-// trace it and return a color. If the ray hits a sphere, we return the color of the
-// sphere at the intersection point, else we return the background color.
-//[/comment]
+
+/// Main rendering function. We compute a camera ray for each pixel of the image
+/// trace it and return a color. If the ray hits a sphere, we return the color of the
+/// sphere at the intersection point, else we return the background color.
+/// @param spheres - the spheres to render.
 void render(const std::vector<Sphere> &spheres)
 {
-    unsigned width = 640, height = 480;
+    float width = 640.0f, height = 480.0f;
     Vec3f *image = new Vec3f[width*height], *pixel = image;
-    float invWidth = 1.0f / float(width), invHeight = 1.0f / float(height);
+    float invWidth = 1.0f/float(width), invHeight = 1.0f/float(height);
     float fov = 30, aspectratio = width/float(height);
-    float angle = tan(M_PI*0.5*fov/180.);
-    //trace the rays
-    for (unsigned y = 0; y<height; ++y)
-    {
-        for (unsigned x = 0; x<width; ++x, ++pixel)
-        {
-            float xx = (2*((x+0.5)*invWidth)-1) * angle * aspectratio;
-            float yy = (1-2*((y+0.5)*invHeight))*angle;
+    float angle = tan(M_PI*0.5*fov/180.0);
+    //trace the rays for each pixel that has to be on screen
+    for (unsigned y = 0; y < height; ++y){
+        for (unsigned x = 0; x < width; ++x, ++pixel){
+            //Mapping coordinates to camera space
+            float xx = (2 * ((x + 0.5) * invWidth) - 1) * angle * aspectratio;
+            float yy = (1 - 2 * ((y + 0.5) * invHeight)) * angle;
+            //Building the ray direction
             Vec3f raydir(xx,yy,-1);
             raydir.normalize();
             *pixel = trace(Vec3f(0),raydir, spheres, 0);
         }
     }
+    //Savign the result to a PPM image
     std::ofstream ofs("./untitled.ppm", std::ios::out | std::ios::binary);
     ofs << "P6\n" << width << " " << height << "\n255\n";
     for (unsigned i = 0; i < width * height; ++i) {
@@ -219,9 +221,9 @@ void render(const std::vector<Sphere> &spheres)
 //[/comment]
 int main(int argc, char **argv)
 {
-    std::vector<Sphere> spheres;
+    std::vector<Sphere> spheres; //Store the spheres in a vector
     // position, radius, surface color, reflectivity, transparency, emission color
-    spheres.push_back(Sphere(Vec3f( 0.0, -10004, -20), 10000, Vec3f(0.20, 0.20, 0.20), 0, 0.0));
+    spheres.push_back(Sphere(Vec3f( 0.0,     -10004, -20), 10000, Vec3f(0.20, 0.20, 0.20), 0, 0.0)); //The plane holding the spheres
     spheres.push_back(Sphere(Vec3f( 0.0,      0, -20),     4, Vec3f(1.00, 0.32, 0.36), 1, 0.5));
     spheres.push_back(Sphere(Vec3f( 5.0,     -1, -15),     2, Vec3f(0.90, 0.76, 0.46), 1, 0.0));
     spheres.push_back(Sphere(Vec3f( 5.0,      0, -25),     3, Vec3f(0.65, 0.77, 0.97), 1, 0.0));
