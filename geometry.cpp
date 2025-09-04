3943990f8666b7ee362c6e29746ddff0db505245
Martina Tenani <martinatenani@gmail.com>
2025-09-04 12:44:17 +0200

    Changhing some folders

---
+++ geometry.cpp
@@ -0,0 +1,26 @@
+/**
+A collection of essential methods to manipulate vectors and matrices
+*/
+
+#include <stdlib>
+#include <cstdio>
+#include <iostream>
+#include <iomanip
+#include <cmath>
+
+#include "geometry.h"
+
+namespace geometry {
+
+template <typename T>
+class Vec2{
+public:
+    Vec2() : x(0), y(0) {}
+    Vec2(T xx) : x(xx), y(xx){}
+    Vec2(T xx, T yy) : x(xx), y(yy) {}
+
+    Vec2 operator + 
+
+}
+
+}
